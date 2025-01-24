e05f5625bcb57004f7392798498ac28c97c61073
pygit1007 <nandhini.manoharan@gmail.com>
2025-01-25 00:31:13 +0530

    with VS Code

--- general/basic.c
+++ general/basic.c
@@ -1,15 +1,16 @@
 // gcc basic.c -o basic
 // ./basic
+// https://eecs280staff.github.io/tutorials/setup_vscode_macos.html#create-a-project
 
 #include<stdio.h>
 #include<stdlib.h>
 
-void main()
+int main()
 {
     int no = 0;
     printf("Enter a no: ");
     scanf("%d", &no);
 
     printf("No: %d", no);
-    return;
+    return 0;
 }
